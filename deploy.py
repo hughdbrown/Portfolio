5b2253f1fd3446f602489e4767d95fcc11cf6fff
Hugh Brown <hughdbrown@yahoo.com>
2009-06-11 12:55:26 -0400

    Remove deploy.py

--- deploy.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-DEBUG = False
-
-# Make sure it ends in a trailing slash!
-BASE_DIR = os.path.dirname(__file__).replace('\\', '/')
-DJANGO_ROOT = "C:/Python25/Lib/site-packages/django/"
-#DJANGO_ROOT = "/usr/lib/python2.5/site-packages/django/"
-ADMIN_MEDIA_ROOT = DJANGO_ROOT + "contrib/admin/media/"
